15c8e9f41f69beab2d054221ac5ed2289e36fdd2
Markinn <marcosmvg77@gmail.com>
2022-04-12 14:59:52 -0300

    fix: cors

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,13 @@
 import express from "express";
 import { connectDatabase } from "./database";
 import routes from "./routes";
+import cors from "cors";
 
 const app = express();
 
 connectDatabase();
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,8 @@
 import "reflect-metadata";
 import app from "./app";
 import { config } from "dotenv";
-import cors from "cors";
 
 config();
-
-app.use(cors());
 
 const PORT = process.env.PORT || 3000;
 
