2fcb0f762a2c711ff793b46afae662a23edbdb73
Markinn <marcosmvg77@gmail.com>
2022-04-12 12:09:47 -0300

    fix: cors error

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,17 @@
 import "reflect-metadata";
 import app from "./app";
 import { config } from "dotenv";
+import cors from "cors";
 
 config();
+
+const corsOptions = {
+  origin: "*",
+  credentials: true,
+  optionSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
 
 const PORT = process.env.PORT || 3000;
 
